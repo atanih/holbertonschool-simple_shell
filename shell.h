68752bece09c3600928bc0b82c8ff245996f425c
atanih <torresalva09@gmail.com>
2026-04-21 22:41:43 -0400

    Shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+char *read_line(void);
+char **split_line(char *line);
+int execute(char **args);
+int launch(char **args);
+
+#endif
